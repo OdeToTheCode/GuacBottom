46046ec880b6be13d22338169b5f42098ca3eb33
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-12 13:26:57 -0600

    added jquery and bootstrap links in html and wrapped docready function around js body

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,50 @@
-const yoMamma = "ypMamma"
+$(function(){
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'text/plain',
+		'X-RapidAPI-Key': '9987ab57famsh4b33ddf26f77c6bp1af32bjsnc0a5287ef750',
+		'X-RapidAPI-Host': 'mycookbook-io1.p.rapidapi.com'
+	},
+	body: 'https://www.delish.com/cooking/recipe-ideas/recipes/a45382/avocado-egg-boats-recipe/'
+};
+let importedRecipe = 'https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi'
+
+
+getApi()
+
+
+
+
+
+function getApi() {
+
+  fetch(importedRecipe, options)
+	.then(function(response){
+    return response.json();
+  })
+	.then(function(data){
+    console.log(data)
+
+
+    
+
+
+  })
+	
+  
+  
+  
+  
+  .catch(err => console.error(err));
+
+}
+
+
+
+
+
+
+
+})
+
