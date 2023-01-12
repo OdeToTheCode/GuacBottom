75d26b918080914a370de62c04cd6581bf10686c
OdeToTheCode <119718185+OdeToTheCode@users.noreply.github.com>
2023-01-12 14:14:37 -0600

    Added to html, added to css, commented out api

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,42 +1,42 @@
-$(function(){
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'text/plain',
-		'X-RapidAPI-Key': '9987ab57famsh4b33ddf26f77c6bp1af32bjsnc0a5287ef750',
-		'X-RapidAPI-Host': 'mycookbook-io1.p.rapidapi.com'
-	},
-	body: 'https://www.delish.com/cooking/recipe-ideas/recipes/a45382/avocado-egg-boats-recipe/'
-};
-let importedRecipe = 'https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi'
+// $(function(){
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'text/plain',
+// 		'X-RapidAPI-Key': '9987ab57famsh4b33ddf26f77c6bp1af32bjsnc0a5287ef750',
+// 		'X-RapidAPI-Host': 'mycookbook-io1.p.rapidapi.com'
+// 	},
+// 	body: 'https://www.delish.com/cooking/recipe-ideas/recipes/a45382/avocado-egg-boats-recipe/'
+// };
+// let importedRecipe = 'https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi'
 
 
-getApi()
+// getApi()
 
 
 
 
 
-function getApi() {
+// function getApi() {
 
-  fetch(importedRecipe, options)
-	.then(function(response){
-    return response.json();
-  })
-	.then(function(data){
-    console.log(data)
+//   fetch(importedRecipe, options)
+// 	.then(function(response){
+//     return response.json();
+//   })
+// 	.then(function(data){
+//     console.log(data)
 
 
     
 
 
-  })
+//   })
 	
   
   
   
   
-  .catch(err => console.error(err));
+//   .catch(err => console.error(err));
 
 }
 
