085805f838b8631fdd89e6e7330351233e27d726
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-13 14:20:50 -0600

    got content displayed on page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
-$(function(){
 
-let recipeDataArray = JSON.parse(localStorage.getItem('recipeDataArray')) || []
+
+$(function(){
   let recipeDataArray = JSON.parse(localStorage.getItem("recipeDataArray")) || []
 
 
@@ -20,13 +20,14 @@ let recipeDataArray = JSON.parse(localStorage.getItem('recipeDataArray')) || []
 getApi()
   function getApi() {
     fetch(importedRecipe, options)
-      .then(function(response){return response.json(); })
+      .then(function(response){
+      return response.json();
+      })
       .then(function(data){
       const r_name = data[0].name
       // const r_image = data[0].image[1]
-      // const r_image = data[0].image[1]
       const r_ingredients = data[0].ingredients
-      const r_instructions = data[0].instructions;
+      const r_instructions = data[0].instructions[0].steps; //made a change here
       const r_yield =  data[0].yield
       
       const recipeObject = {
@@ -35,23 +36,43 @@ getApi()
       ingredients:r_ingredients,
       instructions:r_instructions,
       yield:r_yield
-     
+
       }
-    console.log(recipeObject)
-  
-  })
+      recipeDataArray.push(recipeObject)
+      let stringed = JSON.stringify(recipeDataArray)
+      localStorage.setItem("recipeDataArray", stringed)
+
+    })
     }
+    renderRecipe()
+  function renderRecipe (){
+  recipeDataArray.forEach(recipe =>{
+    console.log(recipe)
+    let recipeName = $('<h5>').text(recipe.name)
+    $('body').append(recipeName)
+
+  recipe.ingredients.forEach(recipeItem =>{
+      console.log(recipeItem)
+      let ingredientLi = $('<li>').text(recipeItem)
+      $('body').append(ingredientLi)
+    })
+    recipe.instructions.forEach(step=>{
+      let instructionStep = $("<li>").text(step)
+      $('body').append(instructionStep)
+    })
+  })}
+  
+  
+  
   })
 
 
 
 
   /*Goals for 1/12/23:
-
   - recipe and put it in array
   - set to local stroge
   - get from local storage
   - take recipe array put into html dom elements
   - get to display on page
-
   */
